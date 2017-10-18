d7b615a422ee9f12aceaac9f164c4b7b252a379a
009shanshukla <shantanushukla61@gmail.com>
2017-10-19 05:08:46 +0530

    adding bit board

---
+++ init.cpp
@@ -0,0 +1,28 @@
+void initsq120to64()
+{
+	int count = 0, sq;              // count will give value from 0 to 63
+
+	for(int i = 0; i < board_square; i++)            // first assign all value 65 in 64 indexed base
+		sq120tosq64[i] = 65;
+
+	for(int i = 0; i < 64; i++)
+		sq64tosq120[i] = 120;							// first assign all value 120 in 120 indexed base
+
+
+	for(int i = rank_1; i <= rank_8; i++)
+	{
+		for(int j = file_a; j <= file_h; j++)
+		{
+			sq = FR2SQ(j, i);                      // first convert 120 indexed value
+			sq64tosq120[count] = sq;               // store in 120 indexed base array at pos count (0 - 63) 
+			sq120tosq64[sq] = count;               // store in 64 indexed base array at pos sq (0 - 119)
+			count++;
+		}
+	}
+
+}
+
+void all_init()
+{
+	initsq120to64();
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include "global_init.h"
+#include "init.cpp"
+#include "bit_board.cpp"
+using namespace std;
+
+int main()
+{
+	all_init();
+	
+	
+	return 0;
+}
